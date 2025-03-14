0cd4e0aca91c9f42ad08a6f66afa948cba798306
RicoLancheros <rico.lancheros@hotmail.com>
2025-03-13 23:30:56 -0500

    V0.0.0.1 - Inicio del Proyecto - Proyecto iniciado con Springboot, modularizado con Traefik, utilizando Mysql y MongoDB. - Inicio

---
+++ services/user-service/src/main/java/rico/user_service/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.example.userservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping("/")
+    public String helloUser() {
+        return "¡Hola desde el User Service!";
+    }
+}
