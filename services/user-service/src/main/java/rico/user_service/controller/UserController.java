e888b9137b5e2687e1a97bb3d5fba24a2b06370f
RicoLancheros <rico.lancheros@hotmail.com>
2025-03-14 00:19:18 -0500

    V0.0.0.2 - Inicio del Proyecto - Proyecto iniciado
    Inicio de funcionamiento con los servicios:
    
    Los servicios responden correctamente, Se realizan pruebas con User Service, Responde correctamente a el llamado en el puerto 8081: return "¡Hola desde el User Service!";
    
    Despues de probados los puertos, se inicia con la construccion del proyecto

--- services/user-service/src/main/java/rico/user_service/controller/UserController.java
+++ services/user-service/src/main/java/rico/user_service/controller/UserController.java
@@ -1,4 +1,4 @@
-package com.example.userservice.controller;
+package rico.user_service.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
