39b31602a12f85b9c5f61bb0f2ee06aa7b882f28
RicoLancheros <rico.lancheros@hotmail.com>
2025-03-25 23:23:25 -0500

    V0.0.0.3 - Cambio de Seguridad - 2
    Se cambian archivos por seguridad, se crean rachivos temporales y se empieza a generar los controlladores de cada servicio para sus respectivas pruebas

--- services/movie-service/src/main/java/rico/movie_service/controller/ShowtimeController.java
+++ services/movie-service/src/main/java/rico/movie_service/controller/ShowtimeController.java
@@ -1,4 +1,4 @@
-package rico.showtime_service.controller;
+package rico.movie_service.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -6,11 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping("/showtime")
+@RequestMapping("/movie")
 public class UserController {
 
     @GetMapping("/")
     public String helloUser() {
-        return "¡Hola desde el Showtime Service!";
+        return "¡Hola desde Movie Service!";
     }
 }
